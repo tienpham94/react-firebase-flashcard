2c1304cc0165ab2ba36511bf9648e9cab1bf9bba
Tien Pham <r3dsky02@yahoo.com.vn>
2017-12-21 23:57:18 +0200

    Working skeleton

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import Card from './Card/Card'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cards: [
+        {id:1 ,eng:"English",
+      han: "Hanzi", pin: "Pinyin"},
+        {id:2 ,eng:"English",
+      han: "Hanzi2", pin: "Pinyin2"}
+      ],
+      currentCard: {}
+    }
+  }
+
+  componentWillMount(){
+    const currentCards = this.state.cards;
+
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)]
+    return card
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Card eng={this.state.currentCard.eng} han={this.state.currentCard.han}
+        pin={this.state.currentCard.pin}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Card/Card.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './Card.css'
+
+const Card = (props) => (
+  <div className="card-container">
+    <div className="card">
+      <div className="front">
+        <div className="eng">
+          {props.eng}
+        </div>
+      </div>
+      <div className="back">
+        <div className="eng">
+          {props.han}
+        </div>
+        <div className="eng">
+          {props.pin}
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Card
