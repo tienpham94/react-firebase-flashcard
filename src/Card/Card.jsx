01d4ae9ccbd9fa7c2bb20e1c46a621db1019cea0
Tien Pham <r3dsky02@yahoo.com.vn>
2017-12-22 21:15:36 +0200

    Restyled

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import './Card.css'
+import './Card.css';
 
 const Card = (props) => (
-  <div className="card-container">
-    <div className="card">
-      <div className="front">
-        <div className="eng">
-          {props.eng}
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="eng">{props.eng}</div>
+            </div>
+            <div className="front back">
+                <div className="fin">{props.fin}</div>
+            </div>
         </div>
-      </div>
-      <div className="back">
-        <div className="eng">
-          {props.han}
-        </div>
-        <div className="eng">
-          {props.pin}
-        </div>
-      </div>
     </div>
-  </div>
 )
 
 export default Card
